ea63769f6f54259d577a00182b5d09d5dd0d2ee6
Tanaka Ayumu <shibibi247@gmail.com>
2018-05-19 17:26:44 +0900

    no message

--- Assets/GetTime.cs
+++ Assets/GetTime.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using UnityEngine.UI;
 
 public class GetTime : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private GameObject timerText;
+    // Use this for initialization
+    void Start () {
+        timerText = GameObject.Find("TimerText");
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        int y = DateTime.Now.Year;
+        int d = DateTime.Now.Day;
+        int h = DateTime.Now.Hour;
+        int m = DateTime.Now.Minute;
+        int s = DateTime.Now.Millisecond;
+
+        
+        timerText.GetComponent<Text>().text = DateTime.Now.ToString("yyyy年MM月dd日hh時mm分ss秒");
+    }
 }
